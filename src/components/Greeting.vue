7bbf3596675d3047ca4dacc7ace6064c34f3b10a
nurvandwi <nurvandwi20@gmail.com>
2021-10-11 17:17:39 +0700

    first

---
+++ src/components/Greeting.vue
@@ -0,0 +1,25 @@
+<template>
+  <p v-if="age > 25">{{ msg }}</p>
+  <p v-else>You must be {{ age }} years old</p>
+</template>
+
+<script>
+export default {
+  name: "Greeting",
+  props: ["age"],
+  data() {
+    return {
+      msg: "Hello world!",
+    };
+  },
+};
+</script>
+
+
+<style scoped lang="scss">
+$color: red;
+
+p {
+  color: $color;
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,26 @@
+<template>
+  <button type="button" @click="onClickAge">Up date Age</button>
+  <p>The user {{ age }} ready</p>
+  <p>{{ ageDoubled }}</p>
+</template>
+
+<script>
+export default {
+  name: "User",
+  props: ["age"],
+  emits: ["age-change"],
+  computed: {
+    ageDoubled() {
+      return this.age * 2;
+    },
+  },
+  methods: {
+    onClickAge() {
+      this.$emit("age-change", 3);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
